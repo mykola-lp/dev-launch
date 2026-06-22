608e00ac078d5014795891f893da11cd873cda29
mykola-lp <mykola.sukhorab@gmail.com>
2026-06-22 12:26:38 +0300

    chore: install and configure ESLint

--- src/main.js
+++ src/main.js
@@ -1,8 +1,2 @@
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-
-import { setupCounter } from './js/counter.js'
-
 import './css/style.css'
-import './js/menu.js'
+import './js/counter.js'
